1922ec2200ce85f48e72aae8b07cf71892d78037
ibrahimbahcekapili <ibrahimbahcekapili@gmail.com>
2023-03-05 16:26:08 +0300

    Creating project

--- app/src/main/java/com/uskudar/bondi/MainActivity.java
+++ app/src/main/java/com/uskudar/bondi/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Merhaba Türkiye");
+        System.out.println("Serhat ÖZEKES galp");
     }
 }
